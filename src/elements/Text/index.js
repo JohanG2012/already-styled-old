23ea440a8853ea9ba7d6e02ec61860f1c70bd9ec
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-03-23 20:42:10 +0100

    :bug: Add missing semicolon

--- src/elements/Text/index.js
+++ src/elements/Text/index.js
@@ -9,9 +9,9 @@ const statusStyle = css`
 const Text = styled.p`
   color: ${props => (props.error ? props.theme.error : props.theme.primary)};
   text-align: ${props => (props.center ? 'center' : 'left')};
-  font-size: ${props => (props.fontSize ? props.fontSize : 'medium')}
-  font-family: ${props => (props.font ? props.font : 'inherit')}
-    ${props => props.status && statusStyle};
+  font-size: ${props => (props.fontSize ? props.fontSize : 'medium')};
+  font-family: ${props => (props.font ? props.font : 'inherit')};
+  ${props => props.status && statusStyle};
 `;
 
 export default Text;
