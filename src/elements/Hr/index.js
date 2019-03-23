3f232d4c6b41c2d32646245f30dc46128dc2bf53
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2019-03-23 20:04:35 +0100

    :pencil: Fix typo

--- src/elements/Hr/index.js
+++ src/elements/Hr/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Hr = styled.hr`
   color: ${props => (props.color ? props.color : 'black')};
   height: 1px;
-  backgroundcolor: ${props => (props.color ? props.color : 'black')};
+  background-color: ${props => (props.color ? props.color : 'black')};
   border: none;
 `;
 
